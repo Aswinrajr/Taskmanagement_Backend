de4914753b7c0ac99ee118315de7a39b49dbf447
Aswinraj <aswinrajr07@gmail.com>
2024-03-31 00:27:47 +0530

    completed

--- db/database.js
+++ db/database.js
@@ -1,14 +1,33 @@
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+
+// const dbConnect = (req, res) => {
+//   mongoose
+//     .connect(`${process.env.DATABASE_CONNECTION}`)
+//     .then(() => {
+//       console.log("Connected to database");
+//     })
+//     .catch((err) => {
+//       console.log("Error in conncting the datbase", err);
+//     });
+// };
+
+// module.exports = dbConnect;
+
 const mongoose = require("mongoose");
-require("dotenv").config();
 
 const dbConnect = (req, res) => {
   mongoose
-    .connect(`${process.env.DATABASE_CONNECTION}`)
+    .connect(
+      "mongodb+srv://aswinrajr07:yPXdXgF4ThtnerDz@cluster0.xf8fs26.mongodb.net/TaskManagementWebsite",
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    )
     .then(() => {
-      console.log("Connected to database");
+      console.log("connected to the database atlas");
+      // console.log("connected to the database");
     })
     .catch((err) => {
-      console.log("Error in conncting the datbase", err);
+      console.error("Error in connecting the database", err);
     });
 };
 
